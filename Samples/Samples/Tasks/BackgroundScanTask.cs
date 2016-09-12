db02c81e3b92e972e73ef3843347aa3417b57402
Allan Ritchie <allan.ritchie@gmail.com>
2016-09-12 13:33:03 -0400

    Update for release

---
+++ Samples/Samples/Tasks/BackgroundScanTask.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Acr.Ble;
+using Acr.Notifications;
+using Samples.Services;
+
+
+namespace Samples.Tasks
+{
+    public class BackgroundScanTask : IAppLifecycle
+    {
+        readonly IAdapter adapter;
+        readonly IAppSettings settings;
+        readonly INotifications notifications;
+
+
+        public BackgroundScanTask(IAdapter adapter, IAppSettings settings, INotifications notifications)
+        {
+            this.adapter = adapter;
+            this.settings = settings;
+            this.notifications = notifications;
+        }
+
+
+        public void OnForeground()
+        {
+            this.notifications.Badge = 0;
+        }
+
+
+        public void OnBackground()
+        {
+            if (!this.settings.BleServerEnabled)
+                return;
+
+            this.adapter
+                .BackgroundScan(this.settings.BleServerServiceUuid)
+                .Subscribe(x =>
+                {
+                    this.notifications.Badge = this.notifications.Badge + 1;
+                    this.notifications.Send("BLE Device Found", $"A device with service UUID {this.settings.BleServerServiceUuid} was found");
+                });
+        }
+    }
+}
