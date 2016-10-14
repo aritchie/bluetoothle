91b563468bec3b0ea562d65c774f670d917a8b47
Allan Ritchie <allan.ritchie@gmail.com>
2016-10-14 12:43:45 -0400

    [fix][droid] parsing 16/32bit uuids in advertisement packet

--- Acr.Ble.Android/Extensions.cs
+++ Acr.Ble.Android/Extensions.cs
@@ -12,9 +12,16 @@ namespace Acr.Ble
                 return new Guid(uuidBytes);
             
             var id = BitConverter.ToString(uuidBytes).Replace("-", String.Empty);
-            if (id.Length == 4)
-                id = $"0000{id}-0000-1000-8000-00805f9b34fb";
+            switch (id.Length)
+            {
+                case 4:
+                    id = $"0000{id}-0000-1000-8000-00805f9b34fb";
+                    break;
 
+                case 8:
+                    id = $"{id}-0000-1000-8000-00805f9b34fb";
+                    break;
+            }
             return Guid.ParseExact(id, "d");
         }
 
