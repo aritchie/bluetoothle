f791c1f0627b61c4d16ea1682abfaeb592181aa9
Allan Ritchie <allan.ritchie@gmail.com>
2016-12-16 16:33:23 -0500

    scan config

---
+++ Acr.Ble.Interfaces/ScanConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Acr.Ble
+{
+    public class ScanConfig
+    {
+        /// <summary>
+        /// Android Only - Initiates a low powered cycle scan
+        /// </summary>
+        public bool IsLowPoweredScan { get; set; }
+
+
+        /// <summary>
+        /// Filters scan to devices that advertise specified service UUID
+        /// iOS - you must set this to initiate a background scan
+        /// </summary>
+        public Guid? ServiceUuid { get; set; }
+    }
+}
