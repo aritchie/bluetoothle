e37b34c96a246190e7d413f168ae7610bced7155
Allan Ritchie <allan.ritchie@gmail.com>
2017-04-03 21:10:25 -0400

    add extension method

--- Plugin.BluetoothLE.Abstractions/Extensions_Device.cs
+++ Plugin.BluetoothLE.Abstractions/Extensions_Device.cs
@@ -6,33 +6,30 @@ namespace Plugin.BluetoothLE
 {
     public static partial class Extensions
     {
+        public static IObservable<IGattCharacteristic> GetKnownCharacteristics(this IDevice device, Guid serviceUuid, params Guid[] characteristicIds)
+            => device
+                .GetKnownService(serviceUuid)
+                .FirstOrDefaultAsync()
+                .SelectMany(x => x.GetKnownCharacteristics(characteristicIds));
+
+
         public static IObservable<IGattCharacteristic> WhenAnyCharacteristicDiscovered(this IDevice device)
-        {
-            return device.WhenServiceDiscovered().SelectMany(x => x.WhenCharacteristicDiscovered());
-        }
+            => device.WhenServiceDiscovered().SelectMany(x => x.WhenCharacteristicDiscovered());
 
 
         public static IObservable<IGattDescriptor> WhenAnyDescriptorDiscovered(this IDevice device)
-        {
-            return device.WhenAnyCharacteristicDiscovered().SelectMany(x => x.WhenDescriptorDiscovered());
-        }
+            => device.WhenAnyCharacteristicDiscovered().SelectMany(x => x.WhenDescriptorDiscovered());
 
 
         public static bool IsPairingAvailable(this IDevice device)
-        {
-            return device.Features.HasFlag(DeviceFeatures.PairingRequests);
-        }
+            => device.Features.HasFlag(DeviceFeatures.PairingRequests);
 
 
         public static bool IsMtuRequestAvailable(this IDevice device)
-        {
-            return device.Features.HasFlag(DeviceFeatures.MtuRequests);
-        }
+            => device.Features.HasFlag(DeviceFeatures.MtuRequests);
 
 
         public static bool IsReliableTransactionsAvailable(this IDevice device)
-        {
-            return device.Features.HasFlag(DeviceFeatures.ReliableTransactions);
-        }
+            => device.Features.HasFlag(DeviceFeatures.ReliableTransactions);
     }
 }
