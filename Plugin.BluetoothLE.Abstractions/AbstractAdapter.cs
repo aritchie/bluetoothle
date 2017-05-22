9933514035e9f4c6163242aeaf2a3db1c63ef265
Allan Ritchie <allan.ritchie@gmail.com>
2017-05-21 21:58:09 -0400

    AbstractAdapter updates

--- Plugin.BluetoothLE.Abstractions/AbstractAdapter.cs
+++ Plugin.BluetoothLE.Abstractions/AbstractAdapter.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reactive.Linq;
 using Plugin.BluetoothLE.Server;
 
 
@@ -10,76 +8,22 @@ namespace Plugin.BluetoothLE
 {
     public abstract class AbstractAdapter : IAdapter
     {
-        public virtual string DeviceName { get; protected set; }
-        public virtual IGattServer CreateGattServer()
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public virtual AdapterFeatures Features { get; }
-        public virtual IDevice GetKnownDevice(Guid deviceId)
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public virtual AdapterStatus Status { get; }
-        public virtual bool IsScanning { get; }
-        public virtual IEnumerable<IDevice> GetConnectedDevices()
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public virtual IEnumerable<IDevice> GetPairedDevices()
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public virtual IObservable<bool> WhenScanningStatusChanged()
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public IObservable<IScanResult> Scan(ScanConfig config = null)
-        {
-            throw new NotImplementedException();
-        }
-
-
+        public abstract IObservable<bool> WhenScanningStatusChanged();
+        public abstract IObservable<IScanResult> Scan(ScanConfig config = null);
         public abstract IObservable<IScanResult> ScanListen();
+        public abstract IObservable<AdapterStatus> WhenStatusChanged();
 
-
-        public virtual IObservable<AdapterStatus> WhenStatusChanged()
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public virtual IObservable<IDevice> WhenDeviceStatusChanged()
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public virtual void OpenSettings()
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public virtual void SetAdapterState(bool enable)
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public virtual IObservable<IDevice> WhenDeviceStateRestored()
-        {
-            throw new NotImplementedException();
-        }
+        public virtual string DeviceName { get; protected set; }
+        public virtual IGattServer CreateGattServer() => throw new NotImplementedException("CreateGattServer is not supported on this platform");
+        public virtual AdapterFeatures Features { get; protected set; }
+        public virtual AdapterStatus Status { get; protected set; }
+        public virtual bool IsScanning { get; protected set; }
+        public virtual IDevice GetKnownDevice(Guid deviceId) => throw new NotImplementedException("GetKnownDevice is not supported on this platform");
+        public virtual IEnumerable<IDevice> GetConnectedDevices() => throw new NotImplementedException("GetConnectedDevices is not supported on this platform");
+        public virtual IEnumerable<IDevice> GetPairedDevices() => throw new NotImplementedException("GetPairedDevices is not supported on this platform");
+        public virtual IObservable<IDevice> WhenDeviceStatusChanged() => throw new NotImplementedException("WhenDeviceStatusChanged is not supported on this platform");
+        public virtual void OpenSettings() => throw new NotImplementedException("OpenSettings is not supported on this platform");
+        public virtual void SetAdapterState(bool enable) => throw new NotImplementedException("SetAdapterState is not supported on this platform");
+        public virtual IObservable<IDevice> WhenDeviceStateRestored() => Observable.Empty<IDevice>();
     }
 }
