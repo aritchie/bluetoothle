f86abb728be63064dd328ac39e7b6e7a8d6ecc7a
Allan Ritchie <allan.ritchie@gmail.com>
2017-07-01 10:08:47 -0400

    pretty this up

--- Plugin.BluetoothLE.Abstractions/Extensions_Adapter.cs
+++ Plugin.BluetoothLE.Abstractions/Extensions_Adapter.cs
@@ -6,69 +6,28 @@ namespace Plugin.BluetoothLE
 {
     public static partial class Extensions
     {
-        public static IObservable<IScanResult> ScanOrListen(this IAdapter adapter)
-        {
-            return adapter.IsScanning ? adapter.ScanListen() : adapter.Scan();
-        }
+        public static IObservable<IScanResult> ScanOrListen(this IAdapter adapter) => adapter.IsScanning ? adapter.ScanListen() : adapter.Scan();
+        public static bool CanOpenSettings(this IAdapter adapter) => adapter.Features.HasFlag(AdapterFeatures.OpenSettings);
+        public static bool CanViewPairedDevices(this IAdapter adapter) => adapter.Features.HasFlag(AdapterFeatures.ViewPairedDevices);
+        public static bool CanControlAdapterState(this IAdapter adapter) => adapter.Features.HasFlag(AdapterFeatures.ControlAdapterState);
+        public static bool CanPerformLowPoweredScans(this IAdapter adapter) => adapter.Features.HasFlag(AdapterFeatures.LowPoweredScan);
 
 
-        public static bool CanOpenSettings(this IAdapter adapter)
-        {
-            return adapter.Features.HasFlag(AdapterFeatures.OpenSettings);
-        }
+        public static IObservable<IDevice> ScanForUniqueDevices(this IAdapter adapter) => adapter
+            .Scan()
+            .Distinct(x => x.Device.Uuid)
+            .Select(x => x.Device);
 
 
-        public static bool CanViewPairedDevices(this IAdapter adapter)
-        {
-            return adapter.Features.HasFlag(AdapterFeatures.ViewPairedDevices);
-        }
-
-
-        public static bool CanControlAdapterState(this IAdapter adapter)
-        {
-            return adapter.Features.HasFlag(AdapterFeatures.ControlAdapterState);
-        }
-
-
-        public static bool CanPerformLowPoweredScans(this IAdapter adapter)
-        {
-            return adapter.Features.HasFlag(AdapterFeatures.LowPoweredScan);
-        }
-
-
-        public static IObservable<IDevice> ScanForUniqueDevices(this IAdapter adapter)
-        {
-            return adapter
-                .Scan()
-                .Distinct(x => x.Device.Uuid)
-                .Select(x => x.Device);
-        }
-
-
-        public static IObservable<IScanResult> ScanWhenAdapterReady(this IAdapter adapter)
-        {
-            return Observable.Create<IScanResult>(ob =>
-            {
-                IDisposable scan = null;
-                var sub = adapter
-                    .WhenStatusChanged()
-                    .Where(x => x == AdapterStatus.PoweredOn)
-                    .Subscribe(x =>
-                        scan = adapter.Scan().Subscribe(ob.OnNext)
-                    );
-
-                return () =>
-                {
-                    scan?.Dispose();
-                    sub.Dispose();
-                };
-            });
-        }
+        public static IObservable<IScanResult> ScanWhenAdapterReady(this IAdapter adapter) => adapter
+            .WhenStatusChanged()
+            .Where(x => x == AdapterStatus.PoweredOn)
+            .Select(x => adapter.Scan())
+            .Switch();
 
 
         public static IObservable<IScanResult> ScanInterval(this IAdapter adapter, TimeSpan timeSpan)
-        {
-            return Observable.Create<IScanResult>(ob =>
+            => Observable.Create<IScanResult>(ob =>
             {
                 var scanner = adapter
                     .Scan()
@@ -97,6 +56,5 @@ namespace Plugin.BluetoothLE
                     scanner?.Dispose();
                 };
             });
-        }
     }
 }
