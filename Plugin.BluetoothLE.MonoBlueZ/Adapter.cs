77730f7b4500e5cfbcb4b435499858e1be034371
Allan Ritchie <allan.ritchie@gmail.com>
2017-07-04 13:48:08 -0400

    bluez starting point

---
+++ Plugin.BluetoothLE.MonoBlueZ/Adapter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Plugin.BluetoothLE
+{
+    public class Adapter : AbstractAdapter
+    {
+        public override IObservable<bool> WhenScanningStatusChanged()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IObservable<IScanResult> Scan(ScanConfig config = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IObservable<IScanResult> ScanListen()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IObservable<AdapterStatus> WhenStatusChanged()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Plugin.BluetoothLE.MonoBlueZ/AdapterScanner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Plugin.BluetoothLE
+{
+    public class AdapterScanner : IAdapterScanner
+    {
+        public bool IsSupported { get; }
+        public IObservable<IAdapter> FindAdapters()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
