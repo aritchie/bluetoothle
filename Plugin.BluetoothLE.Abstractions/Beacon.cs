406a7fd1bc57e47ad594f2d380db8df4a8c3994e
Allan Ritchie <allan.ritchie@gmail.com>
2017-12-10 13:09:29 -0500

    ibeacon extensions investigation

---
+++ Plugin.BluetoothLE.Abstractions/Beacon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Plugin.BluetoothLE
+{
+    public class Beacon
+    {
+        public Beacon(Guid uuid, ushort major, ushort minor)
+        {
+            this.Uuid = uuid;
+            this.Major = major;
+            this.Minor = minor;
+        }
+
+
+        public Guid Uuid { get; }
+        public ushort Major { get; }
+        public ushort Minor { get; }
+    }
+}
