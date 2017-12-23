5263f8e64625f4efe14ab4ffc02253db9ed66436
Allan Ritchie <allan.ritchie@gmail.com>
2017-12-23 13:19:27 -0500

    add stub sample for later

---
+++ Samples/Samples/Tasks/NotificationTask.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Reactive.Linq;
+//using Autofac;
+//using Plugin.BluetoothLE;
+
+
+//namespace Samples.Tasks
+//{
+//    public class NotificationTask : IStartable
+//    {
+//        readonly IAdapter adapter;
+//        readonly INotification notifications;
+
+//        public NotificationTask(IAdapter adapter, INotifications notifications)
+//        {
+//            this.adapter = adapter;
+//            this.notifications = notifications;
+//        }
+
+
+//        public void Start()
+//        {
+//            this.adapter
+//                .WhenStatusChanged()
+//                .Skip(1)
+//                .Where(x => x == AdapterStatus.PoweredOff)
+//                .Subscribe(_ =>
+//                {
+//                    this.notifications.Send("Turn your bluetooth back on!")
+//                });
+//        }
+//    }
+//}
