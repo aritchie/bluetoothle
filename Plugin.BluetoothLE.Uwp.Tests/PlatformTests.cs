c2ef210ac39a8af69202744a69d35e3da17efe21
Allan Ritchie <allan.ritchie@gmail.com>
2017-12-26 08:34:17 -0500

    more fruitless f'ing around with UWP

--- Plugin.BluetoothLE.Uwp.Tests/PlatformTests.cs
+++ Plugin.BluetoothLE.Uwp.Tests/PlatformTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Devices.Bluetooth;
@@ -7,6 +8,7 @@ using Windows.Devices.Bluetooth.Advertisement;
 using Windows.Devices.Bluetooth.GenericAttributeProfile;
 using Windows.Foundation;
 using Xunit;
+using Xunit.Abstractions;
 using GC = Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristic;
 
 
@@ -14,6 +16,15 @@ namespace Plugin.BluetoothLE.Uwp.Tests
 {
     public class PlatformTests
     {
+        readonly ITestOutputHelper output;
+
+
+        public PlatformTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+
         [Fact]
         public async Task ConnectDisconnect()
         {
@@ -35,9 +46,11 @@ namespace Plugin.BluetoothLE.Uwp.Tests
             var bluetoothAddress = await tcs.Task;
             adWatcher.Stop();
             var device = await BluetoothLEDevice.FromBluetoothAddressAsync(bluetoothAddress);
+            this.output.WriteLine($"Bluetooth DeviceId: {device.BluetoothDeviceId.Id} - {device.DeviceId} / {device.Name}");
 
-            var serviceResult = await device.GetGattServicesAsync(BluetoothCacheMode.Uncached);
-            var service = serviceResult.Services.First(x => x.Uuid == Guid.Parse("A495FF20-C5B1-4B44-B512-1370F02D74DE"));
+            var serviceResult = await device.GetGattServicesForUuidAsync(Guid.Parse("A495FF20-C5B1-4B44-B512-1370F02D74DE"));
+            var service = serviceResult.Services.First();
+            await service.OpenAsync(GattSharingMode.Exclusive);
 
             var characteristicResult = await service.GetCharacteristicsAsync(BluetoothCacheMode.Uncached);
             var characteristic = characteristicResult.Characteristics.First();
@@ -50,16 +63,35 @@ namespace Plugin.BluetoothLE.Uwp.Tests
             await characteristic.WriteClientCharacteristicConfigurationDescriptorAsync(GattClientCharacteristicConfigurationDescriptorValue.Notify);
             var data = await chtcs.Task;
 
-            //characteristic.ValueChanged -= handler2;
-            await characteristic.WriteClientCharacteristicConfigurationDescriptorAsync(GattClientCharacteristicConfigurationDescriptorValue.None);
 
+            // start cleanup
+            await characteristic.WriteClientCharacteristicConfigurationDescriptorAsync(GattClientCharacteristicConfigurationDescriptorValue.None);
+            characteristic.ValueChanged -= handler2;
             characteristic = null;
 
-            foreach (var s in serviceResult.Services)
+            service.Dispose();
+            //BluetoothLEDevice.GetDeviceSelectorFromConnectionStatus(BluetoothConnectionStatus.Connected)
+            //service.Session.SessionStatusChanged += (sender, args) =>
+            //{
+            //    //args.Status == GattSessionStatus.Active
+            //};
+            //service.Session.MaintainConnection = true;
+            //foreach (var s in serviceResult.Services)
+            //{
+            //    s.Session.MaintainConnection = false;
+            //    s.Session.Dispose();
+            //    s.Dispose();
+            //}
+            //service = null;
+
+            var list = device.GetType().GetTypeInfo().GetRuntimeMethods().OrderBy(x => x.Name);
+
+            //var releasers = list.Where(x => x.Name.StartsWith("Release"));
+            //foreach (var releaser in releasers)
+            foreach (var method in list)
             {
-                s.Dispose();
+                this.output.WriteLine($"Name: {method.Name}, Static: {method.IsStatic}, Public: {method.IsPublic}, Private: {method.IsPrivate}, Parameters: {method.GetParameters().Length}");
             }
-            service = null;
 
             device.Dispose();
             device = null;
