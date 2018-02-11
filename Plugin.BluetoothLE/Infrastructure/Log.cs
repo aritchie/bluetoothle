e61d102eaf6adba6ed075e70dc81b73c92fea167
Allan Ritchie <allan.ritchie@gmail.com>
2018-02-11 08:21:20 -0500

    add netstandard 1 back move bluez implementation to separate lib

--- Plugin.BluetoothLE/Infrastructure/Log.cs
+++ Plugin.BluetoothLE/Infrastructure/Log.cs
@@ -7,7 +7,9 @@ namespace Plugin.BluetoothLE.Infrastructure
     {
         static Log() => ToDebug();
 
+#if !NETSTANDARD1_0
         public static void ToConsole() => Out = (cat, msg, level) => Console.WriteLine($"[{level}][{cat}] {msg}");
+#endif
         public static void ToDebug() => Out = (cat, msg, level) => System.Diagnostics.Debug.WriteLine($"[{level}][{cat}] {msg}");
 
         public static LogLevel MinLogLevel { get; set; } = LogLevel.Info;
