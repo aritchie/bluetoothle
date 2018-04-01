79e4a4a1a958164cf94ce713e95868d65b4f8bb4
Allan Ritchie <allan.ritchie@gmail.com>
2018-04-01 11:30:51 -0400

    minor

--- Plugin.BluetoothLE/Platforms/Uwp/AdvertisementData.cs
+++ Plugin.BluetoothLE/Platforms/Uwp/AdvertisementData.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Devices.Bluetooth.Advertisement;
 
 
@@ -10,6 +9,7 @@ namespace Plugin.BluetoothLE
     public class AdvertisementData : IAdvertisementData
     {
         readonly BluetoothLEAdvertisementReceivedEventArgs adData;
+        readonly Lazy<Guid[]> serviceUuids;
         readonly Lazy<byte[]> manufacturerData;
         readonly Lazy<int> txPower;
 
@@ -19,7 +19,7 @@ namespace Plugin.BluetoothLE
             this.adData = args;
 
             this.manufacturerData = new Lazy<byte[]>(() => args.Advertisement.GetManufacturerSpecificData());
-            this.ServiceUuids = args.Advertisement.ServiceUuids.ToArray();
+            this.serviceUuids = new Lazy<Guid[]>(() => args.Advertisement.ServiceUuids.ToArray());
             this.txPower = new Lazy<int>(() => args.Advertisement.GetTxPower());
         }
 
@@ -29,24 +29,10 @@ namespace Plugin.BluetoothLE
         public string LocalName => this.adData.Advertisement.LocalName;
         public bool IsConnectable => this.adData.AdvertisementType == BluetoothLEAdvertisementType.ConnectableDirected ||
                                      this.adData.AdvertisementType == BluetoothLEAdvertisementType.ConnectableUndirected;
-        public IReadOnlyList<byte[]> ServiceData { get; }
+
+        public IReadOnlyList<byte[]> ServiceData { get; } = null;
         public byte[] ManufacturerData => this.manufacturerData.Value;
-        public Guid[] ServiceUuids { get; }
+        public Guid[] ServiceUuids => this.serviceUuids.Value;
         public int TxPower => this.txPower.Value;
     }
 }
-/*
-// Manufacturer data - currently unused
-if (btAdv.Advertisement.ManufacturerData.Any())
-{
-foreach (var manufacturerData in btAdv.Advertisement.ManufacturerData)
-{
-// Print the company ID + the raw data in hex format
-//var manufacturerDataString = $"0x{manufacturerData.CompanyId.ToString("X")}: {BitConverter.ToString(manufacturerData.Data.ToArray())}";
-//Debug.WriteLine("Manufacturer data: " + manufacturerDataString);
-var manufacturerDataArry = manufacturerData.Data.ToArray();
-if (manufacturerData.CompanyId == 0x4C && manufacturerData.Data.Length >= 23 &&
-manufacturerDataArry[0] == 0x02)
-{
-}
-}*/
