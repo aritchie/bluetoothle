6ac5bd5656270530d304f68aca66b95afc58b66a
Allan Ritchie <allan.ritchie@gmail.com>
2018-04-22 14:49:53 -0400

    fix deploy script

--- push.cmd
+++ push.cmd
@@ -1,4 +1,5 @@
 @echo off
 copy *.nupkg %HOMEPATH%\dropbox\nuget\ /y
 nuget push .\Plugin.BluetoothLE\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
+del .\Plugin.BluetoothLE\bin\Release\*.nupkg
 pause
