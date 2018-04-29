a039e2aa82fb887b019aae2f319f0b9ab22a166c
Allan Ritchie <allan.ritchie@gmail.com>
2018-04-29 08:02:22 -0400

    fix #177 getknowncharacteristic results not returning due to internal queuing (may have to limit scope of this function in the future)

--- Plugin.BluetoothLE/Platforms/Android/GattService.cs
+++ Plugin.BluetoothLE/Platforms/Android/GattService.cs
@@ -25,27 +25,21 @@ namespace Plugin.BluetoothLE
         }
 
 
-        IObservable<IGattCharacteristic> characteristicOb;
         public override IObservable<IGattCharacteristic> DiscoverCharacteristics()
-        {
-            this.characteristicOb = this.characteristicOb ?? Observable.Create<IGattCharacteristic>(ob =>
+            => Observable.Create<IGattCharacteristic>(ob =>
             {
                 foreach (var nch in this.native.Characteristics)
                 {
                     var wrap = new GattCharacteristic(this, this.context, nch);
                     ob.OnNext(wrap);
                 }
+                ob.OnCompleted();
                 return Disposable.Empty;
-            })
-            .Replay()
-            .RefCount();
-
-            return this.characteristicOb;
-        }
+            });
 
 
-        public override IObservable<IGattCharacteristic> GetKnownCharacteristics(params Guid[] characteristicIds) => this.context.Invoke(
-            Observable.Create<IGattCharacteristic>(ob =>
+        public override IObservable<IGattCharacteristic> GetKnownCharacteristics(params Guid[] characteristicIds)
+            => Observable.Create<IGattCharacteristic>(ob =>
             {
                 var cids = characteristicIds.Select(x => x.ToUuid()).ToArray();
                 foreach (var cid in cids)
@@ -57,8 +51,7 @@ namespace Plugin.BluetoothLE
                 ob.OnCompleted();
 
                 return Disposable.Empty;
-            })
-        );
+            });
 
 
         public override bool Equals(object obj)
