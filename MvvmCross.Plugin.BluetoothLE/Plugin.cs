5997d8e2d57451c637bae27f35078085124a383f
Allan Ritchie <allan.ritchie@gmail.com>
2018-04-29 14:50:55 -0400

    return of the mvx plugin

---
+++ MvvmCross.Plugin.BluetoothLE/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Plugin.BluetoothLE;
+
+
+namespace MvvmCross.Plugin.BluetoothLE
+{
+    [MvxPlugin]
+    [Preserve(AllMembers = true)]
+    public class Plugin : IMvxPlugin
+    {
+        public void Load()
+        {
+            Mvx.RegisterType(() => CrossBleAdapter.Current);
+            Mvx.RegisterType(() => CrossBleAdapter.AdapterScanner);
+        }
+    }
+}
--- push.cmd
+++ push.cmd
@@ -2,4 +2,7 @@
 copy *.nupkg %HOMEPATH%\dropbox\nuget\ /y
 nuget push .\Plugin.BluetoothLE\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
 del .\Plugin.BluetoothLE\bin\Release\*.nupkg
+
+nuget push .\MvvmCross.Plugin.BluetoothLE\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
+del .\MvvmCross.Plugin.BluetoothLE\bin\Release\*.nupkg
 pause
