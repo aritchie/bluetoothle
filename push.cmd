57bc7f38c8144441c55f123708e76bd794ded652
Allan Ritchie <allan.ritchie@gmail.com>
2018-04-29 14:53:57 -0400

    namespace reserved unfortunately

--- push.cmd
+++ push.cmd
@@ -1,7 +1,8 @@
 @echo off
 copy *.nupkg %HOMEPATH%\dropbox\nuget\ /y
-nuget push .\Plugin.BluetoothLE\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
-del .\Plugin.BluetoothLE\bin\Release\*.nupkg
+
+rem nuget push .\Plugin.BluetoothLE\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
+rem del .\Plugin.BluetoothLE\bin\Release\*.nupkg
 
 nuget push .\MvvmCross.Plugin.BluetoothLE\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
 del .\MvvmCross.Plugin.BluetoothLE\bin\Release\*.nupkg
