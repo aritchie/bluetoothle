918ab35f3bb702fec2bb23c9d5a9ca20f6ad394a
Marios Andreou <mandreou@comand.net>
2018-06-19 17:05:00 -0400

    Implemented GetConnectedDevices(), GetKnownDevice(), GetPairedDevices()

--- Plugin.BluetoothLE/Platforms/Uwp/DeviceContext.cs
+++ Plugin.BluetoothLE/Platforms/Uwp/DeviceContext.cs
@@ -17,7 +17,6 @@ namespace Plugin.BluetoothLE
         readonly IList<NC> subscribers;
         IDisposable keepAlive;
 
-
         public DeviceContext(IDevice device, BluetoothLEDevice native)
         {
             this.syncLock = new object();
@@ -28,7 +27,7 @@ namespace Plugin.BluetoothLE
 
 
         public IDevice Device { get; }
-        public BluetoothLEDevice NativeDevice { get; }
+        public BluetoothLEDevice NativeDevice { get; private set; }
 
 
         void StartKeepAlive()
@@ -49,7 +48,7 @@ namespace Plugin.BluetoothLE
         }
 
 
-        public void Ping() => this.NativeDevice.GetGattServicesAsync(BluetoothCacheMode.Uncached); // fire and forget
+        public void Ping() => this.NativeDevice?.GetGattServicesAsync(BluetoothCacheMode.Uncached); // fire and forget
 
 
         public bool Connect()
@@ -58,10 +57,10 @@ namespace Plugin.BluetoothLE
             return true;
         }
 
-
         public async Task Disconnect()
         {
             this.StopKeepAlive();
+
             foreach (var ch in this.subscribers)
             {
                 try
@@ -74,14 +73,16 @@ namespace Plugin.BluetoothLE
                 }
             }
             this.subscribers.Clear();
-            var result = await this.NativeDevice.GetGattServicesAsync(BluetoothCacheMode.Cached);
-            foreach (var s in result.Services)
-            {
-                s.Session.MaintainConnection = false;
-                s.Dispose();
-            }
+            //var result = await this.NativeDevice?.GetGattServicesAsync(BluetoothCacheMode.Cached);
+            //foreach (var s in result.Services)
+            //{
+            //    s.Session.MaintainConnection = false;
+            //    s.Dispose();
+            //}
+
+            this.NativeDevice?.Dispose();
+            this.NativeDevice = null;
             GC.Collect();
-            //this.NativeDevice.Dispose();
         }
 
 
