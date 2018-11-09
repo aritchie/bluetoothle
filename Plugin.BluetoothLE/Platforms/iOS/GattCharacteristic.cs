79405209985af59f0a9ac06e1284e3a7e26c7346
Allan Ritchie <allan.ritchie@gmail.com>
2018-11-09 07:24:11 -0500

    fix #257 - writewitoutresponse shouldn't be swapping to write when unavailable.  Stub comment IsReadyToWrite implementation for later date

--- Plugin.BluetoothLE/Platforms/iOS/GattCharacteristic.cs
+++ Plugin.BluetoothLE/Platforms/iOS/GattCharacteristic.cs
@@ -1,40 +1,55 @@
 ﻿using System;
-using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using Acr.Reactive;
 using CoreBluetooth;
 using Foundation;
-using UIKit;
 
 
 namespace Plugin.BluetoothLE
 {
     public partial class GattCharacteristic : AbstractGattCharacteristic
     {
-        public override IObservable<CharacteristicGattResult> WriteWithoutResponse(byte[] value) => Observable.Create<CharacteristicGattResult>(ob =>
-        {
-            this.AssertWrite(false);
-
-            if (UIDevice.CurrentDevice.CheckSystemVersion(11, 0))
-            {
-                var type = this.Peripheral.CanSendWriteWithoutResponse
-                    ? CBCharacteristicWriteType.WithoutResponse
-                    : CBCharacteristicWriteType.WithResponse;
-                this.Write(ob, type, value);
-            }
-            else
-            {
-                this.Write(ob, CBCharacteristicWriteType.WithoutResponse, value);
-            }
-            return  Disposable.Empty;
-        });
-
-
-        void Write(IObserver<CharacteristicGattResult> ob, CBCharacteristicWriteType type, byte[] value)
+        public override IObservable<CharacteristicGattResult> WriteWithoutResponse(byte[] value)
         {
             var data = NSData.FromArray(value);
-            this.Peripheral.WriteValue(data, this.NativeCharacteristic, type);
-            ob.Respond(new CharacteristicGattResult(this, value));
+            this.Peripheral.WriteValue(data, this.NativeCharacteristic, CBCharacteristicWriteType.WithoutResponse);
+            return Observable.Return(new CharacteristicGattResult(this, value));
         }
+
+
+        //public override IObservable<CharacteristicGattResult> WriteWithoutResponse(byte[] value)
+        //{
+        //    if (UIDevice.CurrentDevice.CheckSystemVersion(11, 0))
+        //        return this.NewInternalWrite(value);
+
+        //    return Observable.Return(this.InternalWrite(value));
+        //}
+
+
+        //IObservable<CharacteristicGattResult> NewInternalWrite(byte[] value) => Observable.Create<CharacteristicGattResult>(ob =>
+        //{
+        //    EventHandler handler = null;
+        //    if (this.Peripheral.CanSendWriteWithoutResponse)
+        //    {
+        //        ob.Respond(this.InternalWrite(value));
+        //    }
+        //    else
+        //    {
+        //        handler = new EventHandler((sender, args) => ob.Respond(this.InternalWrite(value)));
+        //        this.Peripheral.IsReadyToSendWriteWithoutResponse += handler;
+        //    }
+        //    return () =>
+        //    {
+        //        if (handler != null)
+        //            this.Peripheral.IsReadyToSendWriteWithoutResponse -= handler;
+        //    };
+        //});
+
+
+        //CharacteristicGattResult InternalWrite(byte[] value)
+        //{
+        //    var data = NSData.FromArray(value);
+        //    this.Peripheral.WriteValue(data, this.NativeCharacteristic, CBCharacteristicWriteType.WithoutResponse);
+        //    return new CharacteristicGattResult(this, value);
+        //}
     }
 }
