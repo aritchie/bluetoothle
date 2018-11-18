b46ad0db9d4b79af2a1427b41281d4f6003e78ed
Allan Ritchie <allan.ritchie@gmail.com>
2018-11-17 23:26:35 -0500

    minor

--- push.cmd
+++ push.cmd
@@ -1,5 +1,5 @@
 @echo off
-copy *.nupkg %HOMEPATH%\dropbox\nuget\ /y
+rem copy *.nupkg %HOMEPATH%\dropbox\nuget\ /y
 
 nuget push .\Plugin.BluetoothLE\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
 del .\Plugin.BluetoothLE\bin\Release\*.nupkg
